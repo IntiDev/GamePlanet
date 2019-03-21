3ec1761667ab4af5955a1be0c5b4d0a09a765906
Inti <yeseniase@outlook.it>
2019-03-20 22:03:41 -0600

    Se agrega prueba de routing

--- src/main.js
+++ src/main.js
@@ -2,11 +2,10 @@ firebase.initializeApp(config);
 
 firebase.auth().onAuthStateChanged(function(user) {
     //console.log(user);
-    
     if (user) {
       // User is signed in.
-      document.querySelector("#user_view").style.display = "none";
-      document.querySelector("#login_view").style.display = "block";
+      document.querySelector("#user-view").style.display = "none";
+      document.querySelector("#login-view").style.display = "block";
       
       var user = firebase.auth().currentUser;
 
@@ -16,8 +15,8 @@ firebase.auth().onAuthStateChanged(function(user) {
       }
     } else {
       // No user is signed in.
-      document.querySelector("#user_view").style.display = "block";
-      document.querySelector("#login_view").style.display = "none";
+      document.querySelector("#user-view").style.display = "block";
+      document.querySelector("#login-view").style.display = "none";
     }
 });
   
@@ -34,7 +33,6 @@ function login () {
         var errorMessage = error.message;
 
         console.error("Error: " + errorMessage);
-        
     });
 }
 
@@ -43,10 +41,80 @@ function logout () {
   firebase.auth().signOut().then(function() {
     // Sign-out successful.
     console.log("Regresa pronto");
-    
   }); 
 }
 
+//location.replace()
+// VISTAS
+// home-section
+// profile-section
+// friends-section
 
-let historyObj = window.history;
+// let historyObj = window.history;
 // console.log(historyObj);
+
+// const homePage = document.querySelector('#home-section');
+// const profilePage = document.querySelector('#profile-section');
+//const friendsPage = document.querySelector('#friends-section');
+
+const homePage =  `<div class="row">
+                    <div class="col s12 m6">
+                        <div class="card blue">
+                        <div class="card-content white-text">
+                            <span class="card-title">Card Title</span>
+                            <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                        </div>
+                        <div class="card-action">
+                            <a href="#" class="indigo-text darken-4 dark">Editar</a>
+                            <a href="#" class="red-text">Borrar</a>
+                        </div>
+                        </div>
+                    </div>
+                  </div>`;
+
+
+const profilePage = `<h4 class="center"> PROFILE </h4>
+                        <ul class="collection center"">
+                            <img id="profile-img" src="img/profile_photo.png" alt="" class="circle col s4">
+                            <li class="collection-item avatar">
+                                <span class="title">User name</span>
+                                <p> email@domine.com </p>
+                                <P> pas245&/</P>
+                            </li>
+                        </ul> `;
+const friendsPage = `<ul class="collection center"">
+                        <img id="profile-img" src="img/profile_photo.png" alt="" class="circle col s4">
+                        <li class="collection-item avatar">
+                            <span class="title">User name</span>
+                            <p> email@domine.com </p>
+                            <P> pas245&/</P>
+                        </li>
+                    </ul> `;
+
+const routes = {
+  '/': homePage,
+  '/profile': profilePage,
+  '/friends': friendsPage
+};
+
+console.log(window.location.pathname);
+// console.log(location.hostname);
+console.log(location);
+
+
+
+let contentSection = document.querySelector('#login-view');
+console.log(routes[window.location.pathname]);
+
+//contentSection.innerHTML = routes[window.location.pathname];
+
+let onNavItemClick = (pathName) => {
+  window.history.pushState(
+    {}, 
+    pathName,
+    window.location.origin + pathName
+  );
+
+  contentDiv.innerHTML = routes[pathName];
+}
