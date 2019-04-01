d62445bab8aaa8291818dd0af831aeddef11ddd6
IntiDev <yeseniase@outlook.com>
2019-04-01 10:59:59 -0600

    Se separan templates en archivo aparte

--- src/main.js
+++ src/main.js
@@ -1,152 +1,97 @@
 firebase.initializeApp(config);
 
 function loadSection() {
-  
+
 }//loadSection
 
 window.onload = loadSection;
 
-  firebase.auth().onAuthStateChanged(function (user) {
-    //console.log(user);
-    if (user) {
-      // User is signed in.
-      document.querySelector('#user-view').style.display = 'none';
-      document.querySelector('#login-view').style.display = 'block';
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    document.querySelector('#user-view').style.display = 'none';
+    document.querySelector('#login-view').style.display = 'block';
 
-      let user = firebase.auth().currentUser;
+    let user = firebase.auth().currentUser;
 
-      if (user != null) {
-        let emailId = user.email;
-        //alert('Bienvenid@ ' + emailId);
-      }
-    } else {
-      // No user is signed in.
-      document.querySelector('#user-view').style.display = 'block';
-      document.querySelector('#login-view').style.display = 'none';
+    if (user != null) {
+      let emailId = user.email;
+      //alert('Bienvenid@ ' + emailId);
     }
-  });
-
-  
-  
-
-
-  //location.replace()
-  // VISTAS
-  // home-section
-  // profile-section
-  // friends-section
-
-  // let historyObj = window.history;
-  // console.log(historyObj);
-
-  const homePage = `<div class='row'>
-                    <div class='col s12 m6'>
-                        <div class='card blue'>
-                        <div class='card-content white-text'>
-                            <span class='card-title'>Card Title</span>
-                            <p>I am a very simple card. I am good at containing small bits of information.
-                            I am convenient because I require little markup to use effectively.</p>
-                        </div>
-                        <div class='card-action'>
-                            <a href='#' class='indigo-text darken-4 dark'>Editar</a>
-                            <a href='#' class='red-text'>Borrar</a>
-                        </div>
-                        </div>
-                    </div>
-                  </div>`;
+  } else {
+    // No user is signed in.
+    document.querySelector('#user-view').style.display = 'block';
+    document.querySelector('#login-view').style.display = 'none';
+  }
+});
 
 
-  const profilePage = `<h4 class='center'> PROFILE </h4>
-                        <ul class='collection center''>
-                            <img id='profile-img' src='img/profile_photo.png' alt='' class='circle col s4'>
-                            <li class='collection-item avatar'>
-                                <span class='title'>User name</span>
-                                <p> email@domine.com </p>
-                                <P> pas245&/</P>
-                            </li>
-                        </ul> `;
-  const friendsPage = `<ul class='collection'>
-                      <li class='collection-item avatar'>
-                          <img src='img/friend1_photo.png' alt='' class='circle'>
-                          <span class='title'>User name</span>
-                          <p>
-                              Description
-                          </p>
-                          <a href='#!' class='secondary-content'><i class='material-icons'>grade</i></a>
-                      </li>
-
-                      <li class='collection-item avatar'>
-                          <img src='img/friend2_photo.png' alt='' class='circle'>
-                          <span class='title'>User name</span>
-                          <p>
-                              Description
-                          </p>
-                          <a href='#!' class='secondary-content'><i class='material-icons'>grade</i></a>
-                      </li>
-
-                      <li class='collection-item avatar'>
-                          <img src='img/friend3_photo.png' alt='' class='circle'>
-                          <span class='title'>User name</span>
-                          <p>
-                              Description
-                          </p>
-                          <a href='#!' class='secondary-content'><i class='material-icons'>grade</i></a>
-                      </li>
-                    </ul>  `;
-
-  const routes = {
-    'home': homePage,
-    'profile': profilePage,
-    'friends': friendsPage
-  };
-
-  let contentSection = document.querySelector('#home-section');
-  //contentSection.innerHTML = routes[window.location.pathname];
-  window.history.pushState({}, 'home', cleanPath(window.location.pathname) + 'home');
-  console.log(window.location.pathname.split("/"));
-  contentSection.innerHTML = routes[window.location.pathname.split("/").slice(-1)];
 
 
- 
-  const goHome = document.querySelector('#go-home');
-  const goProfile = document.querySelector('#go-profile');
-  const goFriends = document.querySelector('#go-friends');
 
-  goHome.addEventListener('click', showContent);
-  goProfile.addEventListener('click', showContent);
-  goFriends.addEventListener('click', showContent);
+//location.replace()
+// VISTAS
+// home-section
+// profile-section
+// friends-section
 
-  //console.log(goHome.dataset.name);
+// let historyObj = window.history;
+// console.log(historyObj);
 
-  function cleanPath(url) {
-    let arrayUrl = url.split('/');
-    //console.log(arrayUrl.slice(0,arrayUrl.length -1).join('/') + '/');
-    return arrayUrl.slice(0, arrayUrl.length - 1).join('/') + '/';
+
+const routes = {
+  'home': homePage,
+  'profile': profilePage,
+  'friends': friendsPage
+};
+
+let contentSection = document.querySelector('#home-section');
+//contentSection.innerHTML = routes[window.location.pathname];
+window.history.pushState({}, 'home', cleanPath(window.location.pathname) + 'home');
+console.log(window.location.pathname.split("/"));
+contentSection.innerHTML = routes[window.location.pathname.split("/").slice(-1)];
+
+
+
+const goHome = document.querySelector('#go-home');
+const goProfile = document.querySelector('#go-profile');
+const goFriends = document.querySelector('#go-friends');
+
+goHome.addEventListener('click', showContent);
+goProfile.addEventListener('click', showContent);
+goFriends.addEventListener('click', showContent);
+
+//console.log(goHome.dataset.name);
+
+function cleanPath(url) {
+  let arrayUrl = url.split('/');
+  //console.log(arrayUrl.slice(0,arrayUrl.length -1).join('/') + '/');
+  return arrayUrl.slice(0, arrayUrl.length - 1).join('/') + '/';
+}
+
+function showContent(event) {
+  const dataSection = event.target.dataset.name;
+
+  switch (dataSection) {
+    case 'home':
+      contentSection.innerHTML = routes['home']
+      window.history.pushState({}, 'home', cleanPath(window.location.pathname) + 'home');
+      break; ''
+    case 'profile':
+      contentSection.innerHTML = routes['profile']
+      window.history.pushState({}, 'profile', cleanPath(window.location.pathname) + 'profile');
+      break;
+    case 'friends':
+      contentSection.innerHTML = routes['friends']
+      window.history.pushState({}, 'friends', cleanPath(window.location.pathname) + 'friends');
+      break;
+    default:
+      console.log('Error');
+      break;
   }
 
-  function showContent(event) {
-    const dataSection = event.target.dataset.name;
 
-    switch (dataSection) {
-      case 'home':
-        contentSection.innerHTML = routes['home']
-        window.history.pushState({}, 'home', cleanPath(window.location.pathname) + 'home');
-        break; ''
-      case 'profile':
-        contentSection.innerHTML = routes['profile']
-        window.history.pushState({}, 'profile', cleanPath(window.location.pathname) + 'profile');
-        break;
-      case 'friends':
-        contentSection.innerHTML = routes['friends']
-        window.history.pushState({}, 'friends', cleanPath(window.location.pathname) + 'friends');
-        break;
-      default:
-        console.log('Error');
-        break;
-    }
-
-
-  } //showContent
+} //showContent
 
 
 function logout() {
---
+++ src/templates.js
@@ -0,0 +1,55 @@
+const homePage = `<div class='row'>
+<div class='col s12 m6'>
+    <div class='card blue'>
+    <div class='card-content white-text'>
+        <span class='card-title'>Card Title</span>
+        <p>I am a very simple card. I am good at containing small bits of information.
+        I am convenient because I require little markup to use effectively.</p>
+    </div>
+    <div class='card-action'>
+        <a href='#' class='indigo-text darken-4 dark'>Editar</a>
+        <a href='#' class='red-text'>Borrar</a>
+    </div>
+    </div>
+</div>
+</div>`;
+
+
+const profilePage = `<h4 class='center'> PROFILE </h4>
+    <ul class='collection center''>
+        <img id='profile-img' src='img/profile_photo.png' alt='' class='circle col s4'>
+        <li class='collection-item avatar'>
+            <span class='title'>User name</span>
+            <p> email@domine.com </p>
+            <P> pas245&/</P>
+        </li>
+    </ul> `;
+
+const friendsPage = `<ul class='collection'>
+  <li class='collection-item avatar'>
+      <img src='img/friend1_photo.png' alt='' class='circle'>
+      <span class='title'>User name</span>
+      <p>
+          Description
+      </p>
+      <a href='#!' class='secondary-content'><i class='material-icons'>grade</i></a>
+  </li>
+
+  <li class='collection-item avatar'>
+      <img src='img/friend2_photo.png' alt='' class='circle'>
+      <span class='title'>User name</span>
+      <p>
+          Description
+      </p>
+      <a href='#!' class='secondary-content'><i class='material-icons'>grade</i></a>
+  </li>
+
+  <li class='collection-item avatar'>
+      <img src='img/friend3_photo.png' alt='' class='circle'>
+      <span class='title'>User name</span>
+      <p>
+          Description
+      </p>
+      <a href='#!' class='secondary-content'><i class='material-icons'>grade</i></a>
+  </li>
+</ul>  `;
